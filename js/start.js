94c5525b1b5aa5fe15daa9b3623fc35284d8efba
Pavel <pav-kovrizhko@mail.ru>
2019-09-16 01:12:27 +0300

    Homework #2

---
+++ js/start.js
@@ -0,0 +1,64 @@
+// CLOUD
+var CLOUD_WIDTH = 420;
+var CLOUD_HEIGTH = 270;
+var CLOUD_X = 100;
+var CLOUD_Y = 10;
+var GAP = 10;
+// HISTOGRAM
+var HISTOGRAM_WIDTH = 40;
+var HISTOGRAM_HEIGHT = 150;
+
+var histMarginLeft = CLOUD_X + 30;
+var histGap = 90; // растояние между столбцами
+var histValue = 90; // растояние от пололка
+
+var renderCloud = function (ctx, x, y, color) {
+  ctx.fillStyle = color;
+  ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGTH);
+};
+
+var addText = function (ctx, text, x, y) {
+  ctx.fillStyle = '#000000';
+  ctx.font = '16px PT Mono';
+  ctx.textBaseline = 'hanging';
+  ctx.fillText(text, x, y);
+};
+
+var getMaxElement = function (arr) {
+  var maxElement = arr[0];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] > maxElement) {
+      maxElement = arr[i];
+    }
+  }
+
+  return maxElement;
+};
+
+window.renderStatistics = function (ctx, names, times) {
+  // cloud
+  renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.3)');
+  renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+
+  // text message
+  addText(ctx, 'Ура вы победили!', 120, 30);
+  addText(ctx, 'Список результатов:', 120, 50);
+
+  // histograms
+  var maxTime = getMaxElement(times);
+
+  for (var i = 0; i < names.length; i++) {
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'red';
+    } else {
+      var saturationColor = Math.floor(Math.random() * 100);
+      ctx.fillStyle = 'hsl(240,' + saturationColor + '%, 50%)';
+    }
+    var percentHeight = HISTOGRAM_HEIGHT - HISTOGRAM_HEIGHT * (times[i] / maxTime);
+
+    ctx.fillRect(histMarginLeft + (histGap * i), histValue + percentHeight, HISTOGRAM_WIDTH, HISTOGRAM_HEIGHT - percentHeight);
+    addText(ctx, names[i], histMarginLeft + (histGap * [i]), CLOUD_HEIGTH - 20);
+    addText(ctx, Math.floor(times[i]), histMarginLeft + (histGap * [i]), histValue - 20);
+  }
+};
