71f7a880445c5ef6818684f3f23e71ee94485d0b
Pavel <pav-kovrizhko@mail.ru>
2019-09-18 19:27:11 +0300

    Б20, Б21 18.09.2019

--- js/stat.js
+++ js/stat.js
@@ -1,8 +1,8 @@
 'use strict';
 
-var CLOUD_WIDTH = 420;
-var CLOUD_HEIGTH = 270;
-var CLOUD_X = 100;
+// var CLOUD_WIDTH = 420;
+// var CLOUD_HEIGTH = 270;
+/* var CLOUD_X = 100;
 var CLOUD_Y = 10;
 var GAP = 10;
 // HISTOGRAM
@@ -11,13 +11,22 @@ var HISTOGRAM_HEIGHT = 150;
 
 var histMarginLeft = CLOUD_X + 30;
 var histGap = 90;
-var histValue = 90;
+var histValue = 90;*/
+
+var renderCloud = (function () {
+  var CLOUD_WIDTH = 420;
+  var CLOUD_HEIGTH = 270;
+  return function (ctx, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGTH);
+  };
+})();
 
 
-var renderCloud = function (ctx, x, y, color) {
+/* var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGTH);
-};
+};*/
 
 var addText = function (ctx, text, x, y) {
   ctx.fillStyle = '#000000';
@@ -43,7 +52,47 @@ var getRandomColor = function (ctx, hue, lightness) {
   return ctx.fillStyle;
 };
 
-window.renderStatistics = function (ctx, names, times) {
+(function () {
+  var CLOUD_X = 100;
+  var CLOUD_Y = 10;
+  var GAP = 10;
+  // HISTOGRAM
+  var HISTOGRAM_WIDTH = 40;
+  var HISTOGRAM_HEIGHT = 150;
+
+  var histMarginLeft = CLOUD_X + 30;
+  var histGap = 90;
+  var histValue = 90;
+  window.renderStatistics = function (ctx, names, times) {
+    // cloud
+    renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
+    renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
+
+    // text message
+    addText(ctx, 'Ура вы победили!', 120, 30);
+    addText(ctx, 'Список результатов:', 120, 50);
+
+    // histograms
+    var maxTime = getMaxElement(times);
+
+    for (var i = 0; i < names.length; i++) {
+      if (names[i] === 'Вы') {
+        ctx.fillStyle = 'red';
+      } else {
+        getRandomColor(ctx, 240, 50);
+      }
+      var percentHeight = HISTOGRAM_HEIGHT - HISTOGRAM_HEIGHT * (times[i] / maxTime);
+
+      ctx.fillRect(histMarginLeft + (histGap * i), histValue + percentHeight, HISTOGRAM_WIDTH, HISTOGRAM_HEIGHT - percentHeight);
+
+      addText(ctx, names[i], histMarginLeft + (histGap * [i]), 250);
+      addText(ctx, Math.floor(times[i]), histMarginLeft + (histGap * [i]), histValue - 20 + percentHeight);
+    }
+  };
+})();
+
+
+/* window.renderStatistics = function (ctx, names, times) {
   // cloud
   renderCloud(ctx, CLOUD_X + GAP, CLOUD_Y + GAP, 'rgba(0, 0, 0, 0.7)');
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#ffffff');
@@ -65,9 +114,9 @@ window.renderStatistics = function (ctx, names, times) {
 
     ctx.fillRect(histMarginLeft + (histGap * i), histValue + percentHeight, HISTOGRAM_WIDTH, HISTOGRAM_HEIGHT - percentHeight);
 
-    addText(ctx, names[i], histMarginLeft + (histGap * [i]), CLOUD_HEIGTH - 20);
+    addText(ctx, names[i], histMarginLeft + (histGap * [i]), 250);
     addText(ctx, Math.floor(times[i]), histMarginLeft + (histGap * [i]), histValue - 20 + percentHeight);
   }
-};
+};*/
 
 
