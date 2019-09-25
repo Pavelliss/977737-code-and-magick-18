4aa074507d7d435b206a4f8fdf6435bbdc1b0c82
Pavel <pav-kovrizhko@mail.ru>
2019-09-25 23:04:53 +0300

    variable names fixed

--- js/setup.js
+++ js/setup.js
@@ -1,105 +1,108 @@
 'use strict';
 
-var setupBlock = document.querySelector('.setup');
-var setupSimilarBlock = document.querySelector('.setup-similar');
-var similarListElement = document.querySelector('.setup-similar-list');
-var similarWizardTemplate = document.querySelector('#similar-wizard-template')
+(function () {
+  var setupBlock = document.querySelector('.setup');
+  var setupSimilarBlock = document.querySelector('.setup-similar');
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template')
     .content
     .querySelector('.setup-similar-item');
 
-var USER_NAMES = [
-  'Иван',
-  'Хуан Себастьян',
-  'Мария',
-  'Кристоф',
-  'Виктор',
-  'Юлия',
-  'Люпита',
-  'Вашингтон'
-];
+  var USER_NAMES = [
+    'Иван',
+    'Хуан Себастьян',
+    'Мария',
+    'Кристоф',
+    'Виктор',
+    'Юлия',
+    'Люпита',
+    'Вашингтон'
+  ];
 
-var USER_LASTNAME = [
-  'да Марья',
-  'Верон',
-  'Мирабелла',
-  'Вальц',
-  'Онопко',
-  'Топольницкая',
-  'Нионго',
-  'Ирвинг'
-];
+  var USER_LASTNAMES = [
+    'да Марья',
+    'Верон',
+    'Мирабелла',
+    'Вальц',
+    'Онопко',
+    'Топольницкая',
+    'Нионго',
+    'Ирвинг'
+  ];
 
-var COAT_COLORS = [
-  'rgb(101, 137, 164)',
-  'rgb(241, 43, 107)',
-  'rgb(146, 100, 161)',
-  'rgb(56, 159, 117)',
-  'rgb(215, 210, 55)',
-  'rgb(0, 0, 0)'
-];
+  var COAT_COLORS = [
+    'rgb(101, 137, 164)',
+    'rgb(241, 43, 107)',
+    'rgb(146, 100, 161)',
+    'rgb(56, 159, 117)',
+    'rgb(215, 210, 55)',
+    'rgb(0, 0, 0)'
+  ];
 
 
-var EYES_COLOR = [
-  'black',
-  'red',
-  'blue',
-  'yellow',
-  'green'
-];
+  var EYES_COLORS = [
+    'black',
+    'red',
+    'blue',
+    'yellow',
+    'green'
+  ];
 
-var getRandomElement = function (array) {
-  var number = Infinity;
-  while (number > array.length - 1) {
-    number = Math.floor(Math.random() * 10);
-  }
-  return array[number];
-};
+  var getRandomElement = function (array) {
+    var number = Infinity;
+    while (number > array.length - 1) {
+      number = Math.floor(Math.random() * 10);
+    }
+    return array[number];
+  };
 
-var wizards = [
-  {
-    name: getRandomElement(USER_NAMES),
-    lastName: getRandomElement(USER_LASTNAME),
-    coatColor: getRandomElement(COAT_COLORS),
-    eyesColor: getRandomElement(EYES_COLOR)
-  },
-  {
-    name: getRandomElement(USER_NAMES),
-    lastName: getRandomElement(USER_LASTNAME),
-    coatColor: getRandomElement(COAT_COLORS),
-    eyesColor: getRandomElement(EYES_COLOR)
-  },
-  {
-    name: getRandomElement(USER_NAMES),
-    lastName: getRandomElement(USER_LASTNAME),
-    coatColor: getRandomElement(COAT_COLORS),
-    eyesColor: getRandomElement(EYES_COLOR)
-  },
-  {
-    name: getRandomElement(USER_NAMES),
-    lastName: getRandomElement(USER_LASTNAME),
-    coatColor: getRandomElement(COAT_COLORS),
-    eyesColor: getRandomElement(EYES_COLOR)
-  }
-];
+  var wizards = [
+    {
+      name: getRandomElement(USER_NAMES),
+      lastName: getRandomElement(USER_LASTNAMES),
+      coatColor: getRandomElement(COAT_COLORS),
+      eyesColor: getRandomElement(EYES_COLORS)
+    },
+    {
+      name: getRandomElement(USER_NAMES),
+      lastName: getRandomElement(USER_LASTNAMES),
+      coatColor: getRandomElement(COAT_COLORS),
+      eyesColor: getRandomElement(EYES_COLORS)
+    },
+    {
+      name: getRandomElement(USER_NAMES),
+      lastName: getRandomElement(USER_LASTNAMES),
+      coatColor: getRandomElement(COAT_COLORS),
+      eyesColor: getRandomElement(EYES_COLORS)
+    },
+    {
+      name: getRandomElement(USER_NAMES),
+      lastName: getRandomElement(USER_LASTNAMES),
+      coatColor: getRandomElement(COAT_COLORS),
+      eyesColor: getRandomElement(EYES_COLORS)
+    }
+  ];
 
-setupBlock.classList.remove('hidden');
-setupSimilarBlock.classList.remove('hidden');
+  setupBlock.classList.remove('hidden');
+  setupSimilarBlock.classList.remove('hidden');
 
-var renderWizard = function (wizard) {
-  var wizardElement = similarWizardTemplate.cloneNode(true);
+  var renderWizard = function (wizard) {
+    var wizardElement = similarWizardTemplate.cloneNode(true);
 
-  wizardElement.querySelector('.setup-similar-label')
+    wizardElement.querySelector('.setup-similar-label')
   .textContent = wizard.name + ' ' + wizard.lastName;
 
-  wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
-  wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
+    wizardElement.querySelector('.wizard-coat').style.fill = wizard.coatColor;
+    wizardElement.querySelector('.wizard-eyes').style.fill = wizard.eyesColor;
 
-  return wizardElement;
-};
+    return wizardElement;
+  };
 
-var fragment = document.createDocumentFragment();
-for (var i = 0; i < wizards.length; i++) {
-  fragment.appendChild(renderWizard(wizards[i]));
-}
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < wizards.length; i++) {
+    fragment.appendChild(renderWizard(wizards[i]));
+  }
 
-similarListElement.appendChild(fragment);
+  similarListElement.appendChild(fragment);
+
+}());
